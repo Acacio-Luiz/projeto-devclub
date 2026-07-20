90a6438cd18e4622eb84065446e522c72089ecd8
Acacio Luiz <acacioluiz27@gmail.com>
2026-07-20 00:10:39 -0300

    Ajuste para deploy no Vercel

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,10 +1,14 @@
 import app from "./app.js";
 import connectDatabase from "./databases/connection.js";
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
-connectDatabase();
+async function startServer() {
+  await connectDatabase();
 
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando na porta ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando na porta ${PORT}`);
+  });
+}
+
+startServer();
